02de6b81987b8ac1fa114f54ea241fc8ffedb1df
scarf <greenscarf005@gmail.com>
2023-09-07 21:02:33 +0900

    build(docs): setup starlight

---
+++ docs/astro.config.mjs
@@ -0,0 +1,45 @@
+// @ts-check
+import { defineConfig } from "astro/config"
+import starlight from "@astrojs/starlight"
+
+const github = "https://github.com/cataclysmbnteam/Cataclysm-BN"
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://scarf005.github.io",
+  base: "/Cataclysm-BN",
+  integrations: [
+    starlight({
+      title: "Cataclysm: Bright Nights",
+      defaultLocale: "en",
+      locales: {
+        en: { label: "English" },
+        ko: { label: "한국어", lang: "ko-KR" },
+      },
+      logo: { src: "./src/assets/icon-round.svg" },
+      social: { github, discord: "https://discord.gg/XW7XhXuZ89" },
+      /* https://starlight.astro.build/guides/css-and-tailwind/#color-theme-editor */
+      customCss: ["./src/styles/theme.css"],
+      editLink: { baseUrl: `${github}/edit/upload` },
+      lastUpdated: true,
+      sidebar: [
+        {
+          label: "Tutorial",
+          autogenerate: { directory: "tutorial" },
+        },
+        {
+          label: "Guides",
+          autogenerate: { directory: "guides" },
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+        {
+          label: "Explanation",
+          autogenerate: { directory: "explanation" },
+        },
+      ],
+    }),
+  ],
+})
