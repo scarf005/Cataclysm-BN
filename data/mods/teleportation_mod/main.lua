978e1a9f10ff40a86b3dd2beee09a683623ddf7c
scarf <greenscarf005@gmail.com>
2023-11-16 23:09:50 +0900

    feat: teleport on overmap in lua

--- data/mods/teleportation_mod/main.lua
+++ data/mods/teleportation_mod/main.lua
@@ -363,6 +363,7 @@ mod.teleport_to_target = function(who, anchor, distance, teleporter_list_key, pi
   tostring(mod.station_list[teleporter_list_key[picked_teleporter]][3]) ..
   "now has " .. mod.station_list[teleporter_list_key[picked_teleporter]][2] .. " units of charge left")
   --need exposed debug teleport for this to function
+  gapi.place_player_overmap_at(anchor)
   return 1
 end
 
