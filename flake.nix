b9e8d93d85dd394f7c617e441a1742e163261696
scarf <greenscarf005@gmail.com>
2024-06-29 06:23:01 +0000

    build: use nix flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A Nix-flake-based C/C++ development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell.override
+          {
+            # Override stdenv in order to change compiler:
+            stdenv = pkgs.clangStdenv;
+          }
+          {
+            packages = with pkgs; [
+              clang-tools
+              cmake
+              ninja
+              ccache
+
+              # cppcheck
+              # doxygen
+              # gtest
+              # lcov
+            ]
+            ++ (if pkgs.stdenv.isLinux then [ lldb ] else [ ])
+            ++ (if system == "x86_64-windows" then [ vcpkg vcpkg-tool] else [ ])
+            ++ (if system == "aarch64-darwin" then [ ] else [ gdb ]);
+          };
+      });
+    };
+}
+
